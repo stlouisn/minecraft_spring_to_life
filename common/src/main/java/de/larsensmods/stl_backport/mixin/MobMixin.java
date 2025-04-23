3aaec3766a91559184897eebfe960838cc2df80e
Larsen Rektor <larsen@lrektor.de>
2025-04-23 18:33:25 +0200

    Aligned regular chicken behaviour regarding mating with the new chicken types using mixins

---
+++ common/src/main/java/de/larsensmods/stl_backport/mixin/MobMixin.java
@@ -0,0 +1,16 @@
+package de.larsensmods.stl_backport.mixin;
+
+import net.minecraft.world.entity.Mob;
+import net.minecraft.world.entity.ai.goal.GoalSelector;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+
+@Mixin(Mob.class)
+public abstract class MobMixin {
+
+    @Final
+    @Shadow
+    protected GoalSelector goalSelector;
+
+}
