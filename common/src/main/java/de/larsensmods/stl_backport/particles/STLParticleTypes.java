92e97612f72a58734af3a6d2cee39af39a54f29b
Larsen Rektor <larsen@lrektor.de>
2025-05-16 01:02:46 +0200

    Added log message for consistency between classes

--- common/src/main/java/de/larsensmods/stl_backport/particles/STLParticleTypes.java
+++ common/src/main/java/de/larsensmods/stl_backport/particles/STLParticleTypes.java
@@ -2,6 +2,7 @@ package de.larsensmods.stl_backport.particles;
 
 import com.mojang.serialization.MapCodec;
 import de.larsensmods.regutil.IRegistrationProvider;
+import de.larsensmods.stl_backport.SpringToLifeMod;
 import net.minecraft.core.particles.ColorParticleOption;
 import net.minecraft.core.particles.ParticleType;
 import net.minecraft.network.RegistryFriendlyByteBuf;
@@ -15,6 +16,8 @@ public class STLParticleTypes {
     public static Supplier<ParticleType<ColorParticleOption>> TINTED_LEAVES;
 
     public static void initParticleTypes(IRegistrationProvider provider){
+        SpringToLifeMod.LOGGER.info("Initializing particle types");
+
         //noinspection Convert2Diamond
         TINTED_LEAVES = provider.registerParticleType("tinted_leaves", () -> new ParticleType<ColorParticleOption>(false) {
             @Override
