c879e8a9f4d4d4999ec26ca8b6358d46d12ae639
Larsen Rektor <larsen@lrektor.de>
2025-05-17 17:14:22 +0200

    Made dry grass trade available for the experimental trade set

--- neoforge/src/main/java/de/larsensmods/stl_backport/neoforge/event/GameEvents.java
+++ neoforge/src/main/java/de/larsensmods/stl_backport/neoforge/event/GameEvents.java
@@ -2,6 +2,7 @@ package de.larsensmods.stl_backport.neoforge.event;
 
 import de.larsensmods.stl_backport.SpringToLifeMod;
 import de.larsensmods.stl_backport.block.STLBlocks;
+import net.minecraft.world.entity.npc.VillagerTrades;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.item.trading.ItemCost;
@@ -13,9 +14,11 @@ import net.neoforged.neoforge.event.village.WandererTradesEvent;
 @EventBusSubscriber(modid = SpringToLifeMod.MOD_ID, bus = EventBusSubscriber.Bus.GAME)
 public class GameEvents {
 
+    public static final VillagerTrades.ItemListing DRY_GRASS_TRADE = (trader, random) -> new MerchantOffer(new ItemCost(Items.EMERALD, 1), new ItemStack(STLBlocks.TALL_DRY_GRASS.get()), 12, 0, 0);
+
     @SubscribeEvent
     public static void addWandererTrades(WandererTradesEvent event) {
-        event.getGenericTrades().add((trader, random) -> new MerchantOffer(new ItemCost(Items.EMERALD, 1), new ItemStack(STLBlocks.TALL_DRY_GRASS.get()), 12, 0, 0));
+        event.getGenericTrades().add(DRY_GRASS_TRADE);
     }
 
 }
